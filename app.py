f9196eb2c177aff62e38c431d59caef9242bd9f3
rishabh1208 <rishabhsinghal1215@gmail.com>
2022-05-06 21:16:16 -0700

    added Input form

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 # #POST-used to recieve the data
-# #GET-used to send the data back 
+# #GET-used to send the data back
 
 @app.route('/')
-
 def home():
-    return "hello,world"
+    return render_template("home.html")
 
-app.run(port=3000) #specify the port for the app tp run
+
+@app.route('/form')
+def form():
+    return render_template("form.html")
+
+
+app.run(port=3000)  # specify the port for the app tp run
