38f24a861cf304cc0bf1ad8b46383f0ab034de17
rishabh1208 <rishabhsinghal1215@gmail.com>
2022-05-09 19:26:42 -0700

    change styling in form and added ml code in app.py

--- app.py
+++ app.py
@@ -29,8 +29,7 @@ def form():
 @app.route('/handle_data', methods=['POST', 'GET'])
 def handle_data():
     print("Dataframe format required for Machine Learning prediction")
-    # print("tell", request.form.to_dict())
-    # df = pd.DataFrame([request.form.values()], columns=request.form.keys())
+
     dict = {
         'age': 0,
         'default': 0,
@@ -62,36 +61,18 @@ def handle_data():
     dict["default"] = request.form["default"]
     dict["housing"] = request.form["housing"]
     dict["loan"] = request.form["loan"]
-    # print("dict", dict)
-    # print("dict values", dict.values())
+
     df = pd.DataFrame([dict.values()], columns=['age', 'default', 'balance', 'housing', 'loan', 'job_admin.',
-                                                        'job_blue-collar', 'job_entrepreneur', 'job_housemaid',
-                                                        'job_management', 'job_retired', 'job_self-employed', 'job_services',
-                                                        'job_student', 'job_technician', 'job_unemployed', 'job_unknown',
-                                                        'education_primary', 'education_secondary', 'education_tertiary',
-                                                        'education_unknown'])
+                                                'job_blue-collar', 'job_entrepreneur', 'job_housemaid',
+                                                'job_management', 'job_retired', 'job_self-employed', 'job_services',
+                                                'job_student', 'job_technician', 'job_unemployed', 'job_unknown',
+                                                'education_primary', 'education_secondary', 'education_tertiary',
+                                                'education_unknown'])
     # print(dict)
     prob, pred = processing(df, MODEL_PICKLE_PATH)
     print("prob", prob)
     print("prob", pred)
-    # print("tell", request.form.to_dict())
-    # form_data = request.form["job"]
-    # form_data1 = request.form["education"]
-    # form_data2 = request.form["age"]
-    # form_data3 = request.form["balance"]
-    # form_data4 = request.form["default"]
-    # form_data5 = request.form["housing"]
-    # form_data6 = request.form["loan"]
-    # print("form data", form_data)
-    # print("form data1", form_data1)
-    # print("form data2", form_data2)
-    # print("form data3", form_data3)
-    # print("form data4", form_data4)
-    # print("form data5", form_data5)
-    # print("form data6", form_data6)
     return render_template("form.html")
-    # your code
-    # return a response
 
 
 app.run(port=3001, debug=True)  # specify the port for the app tp run
